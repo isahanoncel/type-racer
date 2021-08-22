cc80561e907dbd35ce44ecefb69378d39bb0c6f2
isahan öncel <isahanoncel@neredekal.com>
2021-08-22 15:53:21 +0300

    design pattern completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import {useState,useEffect} from 'react';
 import './App.css';
+import Coin from './Coin'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  return(
+  <div className={'main-window'}>
+    <div className={"score-area"}>
+      <div>Your Score : 400</div>
+      <div>Top Score : 500</div>
     </div>
-  );
+    <div className={'main-window-top-wrapper'}>
+      <div className={"words"}>
+        <ul>
+          <li>Vehicle</li>
+          <li>Vehicle</li>
+          <li>Vehicle</li>
+          <li>Vehicle</li>
+        </ul>
+      </div>
+
+      <div className={"word-point"}>50 <Coin /> </div>
+    </div>
+
+    <div className={"input-area"}>
+      <input type={"text"} />
+    </div>
+
+  </div>
+  )
 }
 
 export default App;
